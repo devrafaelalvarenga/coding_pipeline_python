f36ecd82a54e884eeb0fcbfdbf0894ab6903d5f1
Rafael Alvarenga <devrafaelalvarenga@gmail.com>
2025-03-30 20:47:41 -0300

    update doc

--- coding_pipeline_python/coding_pipeline_python/extract_data.py
+++ coding_pipeline_python/coding_pipeline_python/extract_data.py
@@ -4,12 +4,17 @@ import json
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
 
-try:
-    response = requests.get(url)
-    data = response.json()
-    if response.status_code == 200:
-        print(data)
-    else:
-        print('Error:', data['Error Message'])
-except requests.exceptions.RequestException as e:
-    raise SystemExit(e)
+
+def get_data(url):
+    """
+    Fetch data from the given URL and return the response.
+    """
+    try:
+        response = requests.get(url)
+        data = response.json()
+        if response.status_code == 200:
+            return data
+        else:
+            return ('Error:', data['Error Message'])
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
