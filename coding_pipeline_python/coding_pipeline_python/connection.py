46324d6e24b88b25a8c1f6fc565bfeac2a966676
Rafael Alvarenga <devrafaelalvarenga@gmail.com>
2025-04-01 22:37:09 -0300

    update connection.py

--- coding_pipeline_python/coding_pipeline_python/connection.py
+++ coding_pipeline_python/coding_pipeline_python/connection.py
@@ -1,10 +1,17 @@
 import sqlite3
 import datetime
 import requests
-import json
+import json as json
+import pandas as pd
+# coding: utf-8
 
-database_name = 'alphavantage.db'
+database = 'alphavantage.db'
+source_table = 'raw_time_series_intraday'
+target_table = 'standard_series_intraday'
 current_time = datetime.datetime.now()
+
+# Replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
 
 # Register adapters and converters for datetime
 sqlite3.register_adapter(datetime.datetime, lambda dt: dt.isoformat())
@@ -12,10 +19,10 @@ sqlite3.register_converter(
     "DATETIME", lambda s: datetime.datetime.fromisoformat(s.decode("utf-8")))
 
 
-class Connection():
+class DBConnection():
     def __init__(self):
         try:
-            self.conn = sqlite3.connect(database_name)
+            self.conn = sqlite3.connect(database)
             self.cur = self.conn.cursor()
         except sqlite3.Error as e:
             print('Erro na conexão:', e)
@@ -53,13 +60,11 @@ class Connection():
         return self.fetchall()
 
 
-class ExtractData(Connection):
+class RawDataExtractor(DBConnection):
     def __init__(self):
-        Connection.__init__(self)
+        DBConnection.__init__(self)
 
     def get_data(self, url):
-        # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-        url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
         try:
             response = requests.get(url)
             data = response.json()
@@ -71,64 +76,123 @@ class ExtractData(Connection):
             raise SystemExit(e)
 
 
-class RawData(Connection):
+class RawDataSource(DBConnection):
     def __init__(self):
-        Connection.__init__(self)
+        DBConnection.__init__(self)
 
     def create_table(self):
         try:
-            sql_create = '''
-                CREATE TABLE IF NOT EXISTS raw_time_series_intraday (
+            sql = f'''
+                CREATE TABLE IF NOT EXISTS {source_table} (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     extract_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                     raw_data TEXT
+
                 )
             '''
-            self.execute(sql_create)
+            self.execute(sql)
             self.commit()
         except sqlite3.Error as e:
             print('Erro na criação da tabela:', e)
 
     def insert(self, data):
         try:
-            sql_insert = '''
-            INSERT INTO raw_time_series_intraday (extract_data, raw_data)
+            sql = f'''
+            INSERT INTO {source_table} (extract_data, raw_data)
             VALUES (?, ?)
             '''
-            self.execute(sql_insert, (current_time, json.dumps(data)))
+            self.execute(sql, (current_time, json.dumps(data)))
             self.commit()
         except sqlite3.Error as e:
             print('Erro na inserção:', e)
 
     def delete(self):
         try:
-            sql_delete = '''
-                DELETE FROM raw_time_series_intraday
+            sql = f'''
+                DELETE FROM {source_table}
             '''
-            self.execute(sql_delete)
+            self.execute(sql)
             self.commit()
         except sqlite3.Error as e:
             print('Erro na deleção:', e)
 
     def drop(self):
         try:
-            sql_drop = '''
-                DROP TABLE IF EXISTS raw_time_series_intraday
+            sql = f'''
+                DROP TABLE IF EXISTS {source_table}
             '''
-            self.execute(sql_drop)
+            self.execute(sql)
             self.commit()
         except sqlite3.Error as e:
             print('Erro na deleção:', e)
 
 
-if __name__ == '__main__':
-    with ExtractData() as extract_data:
-        data = extract_data.get_data(
-            'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo')
-        print(data)
+class RawDataProcessor(DBConnection):
+    def __init__(self):
+        DBConnection.__init__(self)
 
-    with RawData() as raw_data:
-        raw_data.insert(data)
-        # raw_data.delete()
-        # raw_data.create_table()
-        # raw_data.drop()
+    def normalize_raw_table(self, source_table, target_table):
+        try:
+            sql_v = f'''
+                SELECT name FROM sqlite_master WHERE type='table' AND name={target_table}
+            '''
+            self.execute(sql_v)
+            if self.fetchall():
+                print(f"Tabela '{target_table}' já existe.")
+                exit(1)
+
+            sql = f'''
+                SELECT raw_data FROM {source_table}
+            '''
+            self.execute(sql)
+            linhas = self.fetchall()
+
+            todos_df = []
+
+            for (raw_json,) in linhas:
+                dados = json.loads(raw_json)
+
+                meta = dados.get("Meta Data", {})
+                series = dados.get("Time Series (5min)", {})
+                symbol = meta.get("2. Symbol", "N/A")
+                timezone = meta.get("6. Time Zone", "UTC")
+
+                df = pd.DataFrame.from_dict(series, orient='index')
+                df.columns = ["open", "high", "low", "close", "volume"]
+                df.index.name = "timestamp"  # renomeia o índice
+                df.reset_index(inplace=True)
+
+                df[["open", "high", "low", "close"]] = df[[
+                    "open", "high", "low", "close"]].astype(float)
+                df["volume"] = df["volume"].astype(int)
+                df["symbol"] = symbol
+                df["timezone"] = timezone
+
+                todos_df.append(df)
+
+            if todos_df:
+                final_df = pd.concat(todos_df, ignore_index=True)
+                final_df.to_sql(target_table, self.conn,
+                                if_exists="replace", index=False)
+                print(
+                    f"Tabela '{target_table}' criada com {len(final_df)} registros.")
+            else:
+                print("Nenhum dado bruto encontrado para normalizar.")
+
+        except Exception as e:
+            print("Erro ao normalizar dados:", e)
+
+
+if __name__ == '__main__':
+    with RawDataExtractor() as raw_data_extractor:
+        data = raw_data_extractor.get_data(url)
+
+    with RawDataSource() as raw_data_source:
+        raw_data_source.create_table()
+        raw_data_source.insert(data)
+        # raw_data_source.delete()
+        # raw_data_source.drop()
+
+    with RawDataProcessor() as raw_data_processor:
+        raw_data_processor.normalize_raw_table(source_table, target_table)
+        # print(data)
